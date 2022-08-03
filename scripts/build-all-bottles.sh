0c36d118264fa60d16a4f46e4e7319a390c1ba77
pasqu4le <pasqu4le@gmail.com>
2022-08-03 16:33:55 +0200

    tmp

--- scripts/build-all-bottles.sh
+++ scripts/build-all-bottles.sh
@@ -4,38 +4,20 @@
 # SPDX-FileCopyrightText: 2022 Oxhead Alpha
 # SPDX-License-Identifier: LicenseRef-MIT-OA
 
-if [ -z "$1" ] ; then
-    echo "Please call this script with the name of the OS for which to build the bottles."
-    exit 1
-fi
-
 set -euo pipefail
 
 # we don't bottle meta-formulas that contain only services
 formulae=("tezos-accuser-013-PtJakart" "tezos-accuser-014-PtKathma" "tezos-admin-client" "tezos-baker-013-PtJakart" "tezos-baker-014-PtKathma" "tezos-client" "tezos-codec" "tezos-node" "tezos-sandbox" "tezos-signer")
 
-# tezos-sapling-params is used as a dependency for some of the formulas
-# so we handle it separately.
-# We don't build the bottle for it because it is never updated over time.
-brew install --formula ./Formula/tezos-sapling-params.rb
-
 for f in "${formulae[@]}"; do
   # check if the formula doesn't already have a bottle in its respective release
-  if ./scripts/check-bottle-built.sh "$f" "$1"; then
+  if echo "$f"; then
     # build a bottle
     if ./scripts/build-one-bottle.sh "$f"; then
-      # upload the bottle to its respective release
-      FORMULA_TAG="$(sed -n "s/^\s\+version \"\(.*\)\"/\1/p" "./Formula/$f.rb")"
-      gh release upload "$FORMULA_TAG" "$f"*.bottle.* ||
-        >&2 echo "Bottle for $f couldn't be uploaded to $FORMULA_TAG release."
+      ./scripts/build-one-bottle.sh "aaa-$f" ||
+        echo "Bottle for $f couldn't be uploaded to release."
     else
       >&2 echo "Bottle for $f couldn't be built."
     fi
   fi
 done
-
-brew uninstall ./Formula/tezos-sapling-params.rb
-# opam doesn't always handle the situation when the same library is present for
-# multiple architectures, see https://github.com/ocaml/opam-repository/issues/20941
-# so all dependencies are cleared after bottles builds to avoid errors
-brew autoremove
--- scripts/build-one-bottle.sh
+++ scripts/build-one-bottle.sh
@@ -12,10 +12,12 @@ if [ -z "$1" ] ; then
     exit 1
 fi
 
-brew install --formula --build-bottle "./Formula/$1.rb"
-# Newer brew versions fail when checking for a rebuild version of non-core taps.
-# So for now we skip the check with '--no-rebuild'
-brew bottle --force-core-tap --no-rebuild "./Formula/$1.rb"
-brew uninstall --formula "./Formula/$1.rb"
-# https://github.com/Homebrew/brew/pull/4612#commitcomment-29995084
-mv "$1"*.bottle.* "$(echo "$1"*.bottle.* | sed s/--/-/)"
+if [ "$1" == "tezos-node" ] ; then
+    exit 1
+fi
+
+if [ "$1" == "aaa-tezos-client" ] ; then
+    exit 2
+fi
+
+cat "$1" > "$1.bottle.tmp"
